90f24ff54c0def9d00b606fd35c70af855222a67
IsItLucas? <lucas.galonet.com@gmail.com>
2025-08-21 19:32:03 -0300

    Update script.py

--- 1059871/script.py
+++ 1059871/script.py
@@ -1,3 +1,8 @@
 import os
 
-os.system('notify-send "Alerta" "Processo finalizado com sucesso!"')
+# Nome do arquivo
+nome_arquivo = "teste.txt"
+
+# Abre o arquivo em modo escrita ("w") - cria se não existir
+with open(nome_arquivo, "w") as f:
+    f.write("Conteúdo inicial do arquivo.\n")
