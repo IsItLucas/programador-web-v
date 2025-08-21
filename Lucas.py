dacf670c62bcec7225700db1754f968cc68db8d2
IsItLucas? <lucas.galonet.com@gmail.com>
2025-08-21 15:34:39 -0300

    Delete Lucas.py

--- Lucas.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-import threading
-
-
-def alerta(msg, titulo="Alerta"):
-    ctypes.windll.user32.MessageBoxW(0, msg, titulo, 0x40 | 0)
-
-
-threading.Thread(target=alerta, args=("Mensagem paralela!", "Aviso")).start()
