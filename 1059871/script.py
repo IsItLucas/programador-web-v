6c14d6db0960ce4194eb2b7bed9e9826a8ba188a
IsItLucas? <lucas.galonet.com@gmail.com>
2025-08-21 19:30:27 -0300

    Update script.py

--- 1059871/script.py
+++ 1059871/script.py
@@ -1,12 +1,3 @@
-import tkinter as tk
-from tkinter import messagebox
+import os
 
-# Criar janela principal (oculta)
-root = tk.Tk()
-root.withdraw()  # esconde a janela principal
-
-# Exibir mensagem de alerta
-messagebox.showwarning("Atenção", "Isso é um alerta!")
-
-# Fechar a aplicação
-root.destroy()
+os.system('notify-send "Alerta" "Processo finalizado com sucesso!"')
