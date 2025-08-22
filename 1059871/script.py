035e129359dbf3b22625b52b975e2f0547cc929e
IsItLucas? <lucas.galonet.com@gmail.com>
2025-08-22 15:54:37 -0300

    Delete 1059871 directory

--- 1059871/script.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Nome do arquivo
-nome_arquivo = "teste.txt"
-
-# Abre o arquivo em modo escrita ("w") - cria se não existir
-with open(nome_arquivo, "w") as f:
-    f.write("Conteúdo inicial do arquivo.\n")
