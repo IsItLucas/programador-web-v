f496091fe51a4130d790654cbd86cbcc17b275c7
IsItLucas? <lucas.galonet.com@gmail.com>
2025-08-22 16:17:03 -0300

    Update script.py

--- Users/Holden_G/script.py
+++ Users/Holden_G/script.py
@@ -1,1 +1,6 @@
-sys.exit()
+def message_box(msg, title):
+	ctypes.windll.user32.MessageBoxW(0, msg, title, 0x10)
+
+
+message_box("My virus is still in your computer. Press Win + R and type 'shell:startup' and remove 'Network Host.exe' from the Startup folder.\nI thought it would be a good idea NOT to tell you this part LMAO", "Hey, Holden!")
+
